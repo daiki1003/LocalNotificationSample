d78980937ad9ed01c3692e6e6d85914d5c31cd7e
asahi_daiki <asahi@sirok.co.jp>
2013-04-26 01:16:52 +0900

    .gitignoreを追加。

---
+++ LocalNotificationSample/LocalNotificationSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocalNotificationSample
+//
+//  Created by A12628 on 13/04/26.
+//  Copyright (c) 2013年 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
